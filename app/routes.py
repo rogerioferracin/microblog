f3937393e5fef3e8c6a300695c50d6135b0ec2dd
Rogerio Ferracin <rogerioferracin@gmail.com>
2021-01-20 08:50:41 -0300

    Final aula 9

--- app/routes.py
+++ app/routes.py
@@ -20,8 +20,15 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title="Home Page", posts=posts, form=form)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page = posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page = posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title="Home Page", posts=posts.items, form=form, next_url = next_url, prev_url=prev_url)
 
 ##
 ## Route Login
@@ -75,12 +82,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items, form=form, next_url=next_url, prev_url=prev_url)
 
 ##
 ## Edit Profile
@@ -129,7 +140,9 @@ def follow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
-
+##
+## Unfollow
+##
 @app.route('/unfollow/<username>', methods=['POST'])
 def unfollow(username):
     form = EmptyForm()
@@ -146,3 +159,19 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+##
+## Explore users
+##
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('explore', page = posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page = posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
