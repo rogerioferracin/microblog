546df134ca59ff3e20637567906bb8f7e390bdec
Rogerio Ferracin <rogerioferracin@gmail.com>
2021-01-21 11:28:24 -0300

    Email Support

--- .flaskenv
+++ .flaskenv
@@ -2,8 +2,8 @@ FLASK_APP=microblog.py
 FLASK_DEBUG=1
 FLASK_USE_RELOADER=1
 
-MAIL_SERVER=smtp.googlemail.com
+MAIL_SERVER=smtp.graficanewtimes.com.br
 MAIL_PORT=587
-MAIL_USE_TLS=1
-MAIL_USERNAME=rogerioferracin@gmail.com
-MAIL_PASSWORD=#R4G3p2f1#
+MAIL_USE_TLS=0
+MAIL_USERNAME=artefinal@graficanewtimes.com.br
+MAIL_PASSWORD=1z2x3C4V@5b
--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['artefinal@graficanewtimes.com.br']
 
     ## Pagination
     POSTS_PER_PAGE = 3
